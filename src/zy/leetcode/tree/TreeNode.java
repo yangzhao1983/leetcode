7af3556c3fb6c2b11a6cc735e36ec3e71abf7712
third-eye <kaiser_zhao1983@sohu.com>
2019-09-12 16:35:07 +0800

    add new method

--- src/zy/leetcode/tree/TreeNode.java
+++ src/zy/leetcode/tree/TreeNode.java
@@ -1,5 +1,9 @@
 package zy.leetcode.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by kaiser_zhao on 15/08/2019.
  */
@@ -27,5 +31,68 @@ public class TreeNode {
 
         // out put right
         output(root.right, prefix+"=right=");
+    }
+
+    /**
+     * generate node tree from string separated by comma
+     *
+     * @param s
+     * @return
+     */
+    public static TreeNode generateTree(String s){
+
+        String[] ss = s.split(",");
+        List<Integer> list = new ArrayList<>();
+        for(String item : ss){
+            if(item.equals("null")){
+                list.add(null);
+            }else{
+                list.add(Integer.parseInt(item));
+            }
+        }
+
+        // create queue
+        LinkedList<TreeNode> queue = new LinkedList<>();
+
+        // create head
+        TreeNode head = new TreeNode(list.get(0));
+
+        // put head to queue
+        queue.push(head);
+
+        // get two items from list, if list is out of space, get null, break;
+        // if not null, create leftNode and rightNode
+        // get item from head of queue, attach leftNode and rightNode to it.
+        // of not null, put leftNode and rightNode to queue.
+        int i = 1;
+        while(true){
+            if(i >= list.size()){
+                break;
+            }
+
+            TreeNode curNode = queue.removeFirst();
+
+            if(list.get(i) == null){
+                curNode.left = null;
+            }else{
+                curNode.left = new TreeNode(list.get(i));
+                queue.add(curNode.left);
+            }
+
+            i++;
+            if(i >= list.size()){
+                break;
+            }
+
+            if(list.get(i) == null){
+                curNode.right = null;
+            }else{
+                curNode.right = new TreeNode(list.get(i));
+                queue.add(curNode.right);
+            }
+
+            i++;
+        }
+        return head;
     }
 }
