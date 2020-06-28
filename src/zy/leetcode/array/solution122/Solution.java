3e904eb97a066dc2474029b7c23bf3e643ef7715
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-06-29 06:07:26 +0800

    fast, but need review

---
+++ src/zy/leetcode/array/solution122/Solution.java
@@ -0,0 +1,12 @@
+package zy.leetcode.array.solution122;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <=1) return 0;
+        int ans = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i-1]) ans += prices[i] - prices[i-1];
+        }
+        return ans;
+    }
+}
