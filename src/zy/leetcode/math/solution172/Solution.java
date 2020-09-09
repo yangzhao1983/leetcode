8e867a647391521f659185eb0272b5174698cdd9
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-09-09 21:13:23 +0800

    very fast

---
+++ src/zy/leetcode/math/solution172/Solution.java
@@ -0,0 +1,12 @@
+package zy.leetcode.math.solution172;
+
+public class Solution {
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        while(n > 0){
+            n /= 5;
+            ans +=n;
+        }
+        return ans;
+    }
+}
