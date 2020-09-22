73eba272e66ee1f0e2878ac8703502bde025a516
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-09-22 14:19:51 +0800

    normal, need review

---
+++ src/zy/leetcode/tree/solution968/Solution.java
@@ -0,0 +1,24 @@
+package zy.leetcode.tree.solution968;
+
+import com.sun.tools.internal.ws.wscompile.WsimportOptions;
+import zy.leetcode.tree.TreeNode;
+
+public class Solution {
+    public int minCameraCover(TreeNode root) {
+        int[] array = dfs(root);
+        return array[1];
+    }
+
+    public int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{Integer.MAX_VALUE / 2, 0, 0};
+        }
+        int[] leftArray = dfs(root.left);
+        int[] rightArray = dfs(root.right);
+        int[] array = new int[3];
+        array[0] = leftArray[2] + rightArray[2] + 1;
+        array[1] = Math.min(array[0], Math.min(leftArray[0] + rightArray[1], rightArray[0] + leftArray[1]));
+        array[2] = Math.min(array[0], leftArray[1] + rightArray[1]);
+        return array;
+    }
+}
