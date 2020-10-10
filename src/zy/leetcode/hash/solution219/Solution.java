1bbf5941a5054e340b24de460659599d67b907b0
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-10-10 22:53:46 +0800

    fast

---
+++ src/zy/leetcode/hash/solution219/Solution.java
@@ -0,0 +1,19 @@
+package zy.leetcode.hash.solution219;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k) return true;
+                else map.put(nums[i], i);
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
