7d53ad6638b56d45c146c98260bdd48de2be3c1f
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-10-24 20:07:04 +0800

    very fast

---
+++ src/zy/leetcode/math/solution231/Solution.java
@@ -0,0 +1,12 @@
+package zy.leetcode.math.solution231;
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        while(n != 1){
+            if(n%2!=0) return false;
+            n = n/2;
+        }
+        return true;
+    }
+}
