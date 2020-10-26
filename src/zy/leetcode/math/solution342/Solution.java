4ce87c7d0ba2b90a4b543ec7048f47edd365a257
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-10-26 11:33:24 +0800

    extremely slow

--- src/zy/leetcode/math/solution342/Solution.java
+++ src/zy/leetcode/math/solution342/Solution.java
@@ -1,4 +1,9 @@
 package zy.leetcode.math.solution342;
 
 public class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num < 1) return false;
+        String s= Integer.toString(num, 4);
+        return s.matches("^10*$");
+    }
 }
