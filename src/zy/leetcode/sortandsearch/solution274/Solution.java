fd623d54e6b6b0b8b4d627db1917a86309686d88
yangzhao1983 <kaiser_zhao1983@sohu.com>
2020-11-18 11:41:07 +0800

    normal, need review

---
+++ src/zy/leetcode/sortandsearch/solution274/Solution.java
@@ -0,0 +1,14 @@
+package zy.leetcode.sortandsearch.solution274;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int i = 0;
+        while (i < citations.length && citations[citations.length - 1 - i] > i) {
+            i++;
+        }
+        return i;
+    }
+}
